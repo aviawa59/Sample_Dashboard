766ef3c9757cfd03ffe943864f8a325331eb1f13
aviawa59 <avinashawarad59@gmail.com>
2025-07-17 17:51:56 +0530

    updated the file

---
+++ pyfile/streamlit_dashboard.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="KPTCL Dashboard", page_icon="⚡", layout="wide", initial_sidebar_state="expanded")
+st.title("GESCOM Dashboard")
+
+# ✅ Typo fixed: file_uploader, not file_uploder
+data_file = st.file_uploader("Upload CSV File", type=["csv"])
+
+if data_file is not None:
+    df = pd.read_csv(data_file)
+
+    st.subheader("Data Preview")
+    st.dataframe(df)
+
+    st.subheader("📈 Basic Statistics")
+    st.write(df.describe())
+
+
+
+

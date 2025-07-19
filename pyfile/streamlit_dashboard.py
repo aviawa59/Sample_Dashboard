a1a3b05dd4df905f29b60b8d088acc62376c938c
aviawa59 <avinashawarad59@gmail.com>
2025-07-19 11:38:26 +0530

    Further Changes

--- pyfile/streamlit_dashboard.py
+++ pyfile/streamlit_dashboard.py
@@ -4,11 +4,11 @@ import pandas as pd
 st.set_page_config(page_title="KPTCL Dashboard", page_icon="⚡", layout="wide", initial_sidebar_state="expanded")
 st.title("GESCOM Dashboard")
 
-# ✅ Typo fixed: file_uploader, not file_uploder
 data_file = st.file_uploader("Upload CSV File", type=["csv"])
 
 if data_file is not None:
     df = pd.read_csv(data_file)
+    df.columns = df.columns.str.strip()
 
     st.subheader("Data Preview")
     st.dataframe(df)
@@ -18,15 +18,15 @@ if data_file is not None:
 
     if "Division" in df.columns and "Account ID" in df.columns:
         st.subheader("Division Wise Consumer Count")
-        Division_Count_df = (df.groupby("Division")["Account ID"].nunique.reset_index())
-        st.write(division_count_df)
-        st.bar_chart(data = Division_Count_df, x = "Division", y = "Account ID")
+
+        Division_Count_df = (
+            df.groupby("Division")["Account ID"]
+            .nunique()
+            .reset_index()
+        )
+
+        st.write(Division_Count_df)
+        st.bar_chart(data=Division_Count_df, x="Division", y="Account ID")
     
     else:
         st.warning("Required columns 'Division' or 'Account ID' not found in the uploaded file")
- 
-
-
-
-
-
